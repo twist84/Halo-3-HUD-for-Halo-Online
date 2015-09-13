35897b8c5a6a56bd301571f42bfe8690f5c8b08a
Alex-231 <git+a13x231mcsl@gmal.com>
2015-09-13 18:41:23 +0100

    Fixed Radar Size, Removed H:O Bloat via CHDT, Installer Fixes.

--- Install Halo 3 hud.bat
+++ Install Halo 3 hud.bat
@@ -1,6 +1,30 @@
 @echo off
 title Halo Online Tag Patcher
 echo.
+echo
+echo                            .NMMMMMMMMMMMMMM?                                   
+echo                        MMMMMMMMMMMMMMMMMMMMMMMMMMMMM                           
+echo                    ~MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                      
+echo                .MMMMMMMMMMMMMMMM7~~~$MMMMMMMMMMMMMMMMMMMMMMM                   
+echo             :MMMMMMM7                           IMMMMMMMMMMMMMD                
+echo           MMMMMM            MMMMMMMMMMMMMM           MMMMMMMMMMMN              
+echo         .MMMMZ           MMMMMMMMMMMMMMMMMMMD+         :MMMMMMMMMM             
+echo        MMMM$             M=           MMMM               OMMMMMMMMM            
+echo       MMMM                          ,MMM                   MMMMMMMMM           
+echo  MMMMMMMM                         MMMMMMMMM~                MMMMMMMMM          
+echo   MMMMMM                                 MMMMMM             MMMMMMMMMMMMMMMM   
+echo                                             MMMMO           MMMMMMMMMM         
+echo                     .                       MMMMM          8MMMMMMMMM          
+echo                    MMMM                     MMMMM         =MMMMMMMMM           
+echo                      OMMMMM              MMMMMMM         MMMMMMMMMM            
+echo                         $MMMMMMMMMMMMMMMMMMMMM         MMMMMMMMMMM             
+echo                            MMMMMMMMMMMMMM8          MMMMMMMMMMMM8              
+echo                    MM                           NMMMMMMMMMMMMM8                
+echo                   MMMMMMMMM:             =MMMMMMMMMMMMMMMMMM                   
+echo                      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMZ                      
+echo                           .MMMMMMMMMMMMMMMMMMMMMMMMM                           
+echo                                 MMMMMMMMMMN=                                   
+echo.
 echo.
 echo  Patch     : Halo 3 HUD
 echo.
