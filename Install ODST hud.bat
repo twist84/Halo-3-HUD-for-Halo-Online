51db551203c5df0cad409c50e75241b3bf274139
Alex-231 <git+a13x231mcsl@gmal.com>
2015-09-13 15:19:31 +0100

    Installer Preparation
    Extracted 2 images, stole Khadazi's installer.

---
+++ Install ODST hud.bat
@@ -0,0 +1,30 @@
+@echo off
+title Halo Online Tag Patcher
+echo.
+echo.
+echo  Patch     : Halo 3 HUD
+echo.
+echo  Version   : Pre-Release
+echo.
+echo  Type      : TAG AND TEXTURE REPLACEMENT
+echo.
+echo  Author    : Alex231
+echo.
+echo  Info      : Replaces HUD elements with their Halo 3 versions.
+echo.
+echo.
+echo.
+echo.
+echo  Press any key to start patching...
+echo.
+pause>null
+echo.
+echo.
+Type import.cmds | HaloOnlineTagTool.exe
+echo.
+echo.
+echo.
+echo.
+echo  Patching complete, Press any key to close...
+echo.
+pause>null
